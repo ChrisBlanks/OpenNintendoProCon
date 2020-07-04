2b0da4d06cee1a59bda210b36af70b10e662fad0
ChrisBlanks <cablanks@hotmail.com>
2020-07-03 21:52:21 -0400

    add getJoystickInfo(); update testControllerInputs; edit structs

--- src/pro_con_utils.c
+++ src/pro_con_utils.c
@@ -15,23 +15,104 @@ Date: Summer 2020
 #include "pro_con_errors.h"
 
 
-int getJoystickInfo(struct js_event* event_info,joysticks_t* joystick_data){
+int getButtonInfo(struct js_event* event_info,button_input_t* button_data){
+    button_data->button_code = event_info->number;
+    button_data->isPressed = event_info->value;
+
+    switch(button_data->button_code){
+        case B_BUTTON:
+            button_data->button_name = "B-Button";
+            break;
+        case A_BUTTON:
+            button_data->button_name = "A-Button";
+            break;
+        case Y_BUTTON:
+            button_data->button_name = "Y-Button";
+            break;
+        case X_BUTTON:
+            button_data->button_name = "X-Button";
+            break;
+        case L_BUTTON:
+            button_data->button_name = "L-Button";
+            break;     
+        case R_BUTTON:
+            button_data->button_name = "R-Button";
+            break;   
+        case ZL_BUTTON:
+            button_data->button_name = "ZL Button";
+            break;
+        case ZR_BUTTON:
+            button_data->button_name = "ZR Button";
+            break;
+        case MINUS_BUTTON:
+            button_data->button_name = "Minus Button";
+            break;
+        case PLUS_BUTTON:
+            button_data->button_name = "Plus Button";
+            break;
+        case LEFT_JS_PRESS:
+            button_data->button_name = "Left Joystick";
+            break;
+        case RIGHT_JS_PRESS:
+            button_data->button_name = "Right Joystick";
+            break;
+        case HOME_BUTTON:
+            button_data->button_name = "Home Button";
+            break;
+        case SCREENSHOT_BUTTON:
+            button_data->button_name = "Screenshot Button";
+            break;
+        default:
+            break;
+    }
+
+    return SUCCESSFUL_EXECUTION;
+}
+
+
+int getJoystickInfo(struct js_event* event_info,direction_input_t* joystick_data){
 
     joystick_data->joysticks_pos[event_info->number] = event_info->value;
     joystick_data->changed_axis = event_info->number;
 
+    joystick_data->direction_type = (joystick_data->changed_axis >= DIR_PAD_HORIZ)   ? "Arrow"  : "Stick";
+
     switch(joystick_data->changed_axis){
         case LEFT_JOY_HORIZ_AXIS:
+            joystick_data->direction = "Left";
             joystick_data->other_axis = LEFT_JOY_VERT_AXIS;
             break;
         case LEFT_JOY_VERT_AXIS:
+            joystick_data->direction = "Left";
             joystick_data->other_axis = LEFT_JOY_HORIZ_AXIS;
             break;
         case RIGHT_JOY_HORIZ_AXIS:
+            joystick_data->direction = "Right";
             joystick_data->other_axis = RIGHT_JOY_VERT_AXIS;
             break;
         case RIGHT_JOY_VERT_AXIS:
+            joystick_data->direction = "Right";
             joystick_data->other_axis = RIGHT_JOY_HORIZ_AXIS;
+            break;
+        case DIR_PAD_HORIZ:
+            if(event_info->value == 0){
+                joystick_data->direction = "Idle";
+                joystick_data->direction_type = "Pad";
+            } else{
+                joystick_data->direction = event_info->value < 0 ? "Left" : "Right";
+            }
+
+            joystick_data->other_axis = DIR_PAD_VERT;
+            break;
+        case DIR_PAD_VERT:
+            if(event_info->value == 0){
+                joystick_data->direction = "Idle";
+                joystick_data->direction_type = "Pad";
+            } else{
+                joystick_data->direction = event_info->value < 0 ? "Up" : "Down";
+            }
+            
+            joystick_data->other_axis = DIR_PAD_HORIZ;
             break;
         default:
             break; //no other axes
@@ -58,13 +139,15 @@ int testControllerInputs(int joystick_fd,char* joystick_file_name){
     int status = SUCCESSFUL_EXECUTION;
     __u32 driver_vers = 0;
 
-    char* joystick    = NULL;
+    char* direction      = NULL,
+        * direction_type = NULL;
 
     struct js_event cur_event = {0};
-    joysticks_t controler_js = {0};
+    direction_input_t controler_js   = {0};
+    button_input_t controller_button = {0};
     
     fprintf(stdout,"\n------Starting Nintendo Pro Controller Testing------\n");  
-    fprintf(stdout,"Joystick Device @ %s\n",joystick_file_name);
+    fprintf(stdout,"Controller Device @ %s\n",joystick_file_name);
 
     //process & print events
     while( status == SUCCESSFUL_EXECUTION ){ 
@@ -72,26 +155,22 @@ int testControllerInputs(int joystick_fd,char* joystick_file_name){
         if(status == DEVICE_READ_ERROR){ continue; }
 
         switch(cur_event.type){
-            case JS_EVENT_BUTTON:
-                fprintf(stdout,"\nJoystick Event: %u %s\n",
-                        cur_event.number,
-                        cur_event.value ? "pressed": "released"
-                       ); //add CLI for file logging enable
+            case KEY_BUTTON_TYPE:
+                getButtonInfo(&cur_event,&controller_button);
+                fprintf(stdout,"\nJoystick Event: %s %s\n",
+                        controller_button.button_name,
+                        controller_button.isPressed ? "pressed": "released"
+                       ); 
                 break;
-            case JS_EVENT_AXIS:
-                status = getJoystickInfo(&cur_event,&controler_js);
-                if(status != SUCCESSFUL_EXECUTION){
-                    fprintf(stderr,"\nError(%d): %s\n",status,getErrorMessage(status));
-                    return status;
-                }
-                
-                joystick =  (controler_js.changed_axis < 2) ? "Left" : "Right" ;
-                fprintf(stdout,"\nJoystick Event: %s Joystick @ position (%d, %d)\n",
-                        joystick,
+            case JOYSTICK_TYPE:
+                getJoystickInfo(&cur_event,&controler_js);
+
+                fprintf(stdout,"\nJoystick Event: %s %s @ position (%d, %d)\n",
+                        controler_js.direction,
+                        controler_js.direction_type,
                         controler_js.joysticks_pos[controler_js.changed_axis],
                         controler_js.joysticks_pos[controler_js.other_axis]
                        ); //add CLI for file logging enable
-                
                 break;
 
             default:
--- src/pro_con_utils.h
+++ src/pro_con_utils.h
@@ -16,25 +16,63 @@ Date: Summer 2020
 #define CLI_ARGS_EXPECTED 2
 #define JOYSTICK_DEV 1
 
-#define NUM_OF_AXES 4
+#define NUM_OF_AXES 6
 
 
 //data structure definitions 
-typedef struct joysticks_t {
+
+typedef struct button_input_t{
+    int button_code;
+    int isPressed;
+
+    char* button_name;
+} button_input_t;
+
+typedef struct direction_input_t {
     int joysticks_pos[NUM_OF_AXES];
     int changed_axis;
     int other_axis;
-} joysticks_t;
 
-enum event_type_map{
+    char* direction;
+    char* direction_type;
+} direction_input_t;
+
+
+// enum definitions 
+enum controller_event_type {
+    KEY_BUTTON_TYPE   = JS_EVENT_BUTTON,
+    JOYSTICK_TYPE     = JS_EVENT_AXIS
+};
+
+enum js_event_type_map{
     LEFT_JOY_HORIZ_AXIS  = 0,
     LEFT_JOY_VERT_AXIS   = 1,  
     RIGHT_JOY_HORIZ_AXIS = 2,
-    RIGHT_JOY_VERT_AXIS  = 3,   
+    RIGHT_JOY_VERT_AXIS  = 3,
+    DIR_PAD_HORIZ        = 4,
+    DIR_PAD_VERT         = 5   
+};
+
+enum key_event_type_map{
+    B_BUTTON          = 0,
+    A_BUTTON          = 1,
+    Y_BUTTON          = 2,
+    X_BUTTON          = 3,
+    L_BUTTON          = 4,
+    R_BUTTON          = 5,
+    ZL_BUTTON         = 6,
+    ZR_BUTTON         = 7,
+    MINUS_BUTTON      = 8,
+    PLUS_BUTTON       = 9,
+    LEFT_JS_PRESS     = 10,
+    RIGHT_JS_PRESS    = 11,
+    HOME_BUTTON       = 12,
+    SCREENSHOT_BUTTON = 13  
 };
 
 //function prototpyes
-int getJoystickInfo(struct js_event* event_info,joysticks_t* joystick_data);
+int getButtonInfo(struct js_event* event_info,button_input_t* button_data);
+int getJoystickInfo(struct js_event* event_info,direction_input_t* joystick_data);
 int readControllerEvent(int joystick_fd, struct js_event* event_info);
 int testControllerInputs(int joystick_fd,char* joystick_file_name);
 
