95735ff16b980c9fe1cf5ffc93314b8d6dc1dd42
ChrisBlanks <cablanks@hotmail.com>
2020-10-07 23:13:48 -0400

    add defines for app installation values and test path

--- src/config.h
+++ src/config.h
@@ -12,6 +12,12 @@ Date: Summer 2020
 #define CLI_ARGS_EXPECTED 2  //executing file name & default joystick path
 #define JOYSTICK_ARG 1
 
+#define APP_NAME "openprocon"
+#define LOCAL_SHARE_DIR ".local/share"
+#define DEFAULT_RESOURCE_DIR LOCAL_SHARE_DIR "/" APP_NAME
+
+#define TEST_JOYSTICK_PATH "/dev/input/js0"
+
 #define SOFTWARE_MAJOR_VERSION  0
 #define SOFTWARE_MINOR_VERSION  1
 #define SOFTWARE_PATCH_VERSION  0
